8fb2b95e6db2983a2ef1b38f5bf45efefd9abe55
haayd <haydee.sramirez@gmail.com>
2022-06-02 15:55:11 -0700

    the state of the game, render to the user.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-const players 
-
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -18,7 +26,7 @@ const squareEls = document.querySelectorAll(".square")
 //console.log(squareEls)
 
 const messageEl = document.querySelector("h2")
-console.log(messageEl)
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -29,12 +37,34 @@ console.log(messageEl)
 init()
 
 function init() {
-  board = [null, null, null, null, null, null, null, null, null]
+  board = [null, -1, null, null, null, null, null, null, null]
   turn = 1 // represents player x 
   winner = null // null = no winner, 1 = x won, -1 = o won. "T a tie occured, anything else = game over."
+  render ()
 
-
-
-  render()
+  
 }
 
+function render () {
+  board.forEach((square, i)=> {
+   if (square === 1) {
+     squareEls[i].textContent = "X"
+  } 
+   if (square === -1) {
+    squareEls[i].textContent = "O"
+  }
+  
+})  
+  renderMessage()
+} 
+function renderMessage() {
+  if(winner === null) {
+    messageEl.textContent = `It is player ${turn === 1 ? "X": "O"}'s turn!`
+  } else if (winner === "T") {
+    messageEl.textContent = `It's a tie`
+  } else {
+    messageEl.textContent = `Player ${turn === 1 ? "0": "X"} won`
+  }
+
+
+  }
