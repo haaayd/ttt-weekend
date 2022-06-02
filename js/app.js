6a9c2f3c707c5f8e0cd10643afb0a92fe6bf6006
haayd <haydee.sramirez@gmail.com>
2022-06-02 12:37:58 -0700

    create a function, call it init

--- js/app.js
+++ js/app.js
@@ -4,15 +4,26 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board, // to represent the state of the squares on the board 
+turn, // to track whose turn it is 
+winner // to represent if anyone has won yet, or if its a tie 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".square")
+//console.log(squareEls)
 
-
+const messageEl = document.querySelector("h2")
+console.log(messageEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
-/*-------------------------------- Functions --------------------------------*/
 
+/*-------------------------------- Functions --------------------------------*/
+init()
+
+function init() {
+  
+}
